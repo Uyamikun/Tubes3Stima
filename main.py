89c126e61368f5eccc64efd02cade00c6f25b06a
Uyamikun <nabil.nabighah@gmail.com>
2021-04-26 14:14:09 +0700

    printAllSavedTask (no args yet)

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import re
 app = Flask(__name__)
 
-userChat = ["pertama"]
-botChat = ["balesan pertama"]
-chat = [["pertama", 1], ["balesan pertama", 0]]
+chat = []
 arrayTugas = []
 arrayKataPenting = []
 arrayDataMatkul = []
@@ -26,10 +24,10 @@ for item in f:
     for item2 in arrayTemp:
         arrayScrapTopik.append(item2)
 f.close()
-print(arrayScrapTopik)
+#print(arrayScrapTopik)
 
 f = open("database/katapenting.txt", 'r')
-print(arrayDataMatkul)
+#print(arrayDataMatkul)
 for item in f:
     arrayTemp = item.rstrip("\n").rsplit(',')
     for item2 in arrayTemp:
@@ -37,6 +35,20 @@ for item in f:
 f.close()
 
 bulan = ["JANUARI", "FEBRUARI", "MARET", "APRIL", "MEI", "JUNI"]
+
+def balesanBot(s):
+    if(re.search("masuk", s) != None):
+        return "Jadwal telah dimasukkan"
+    else:
+        temp = inputTask(s)
+        if(temp != ""):
+            return temp
+        elif(checkTask(s) != ""):
+            return checkTask(s)
+        else:
+            #inputan kosong print arrayTugas yang tersimpan(temporary)
+            return printAllTask(arrayTugas)
+            
 
 # fungsi untuk bot, yang paling utama adalah fungsi balesan bot, sisanya fungsi pendukung
 def booyer_moore(text, pattern):
@@ -58,7 +70,7 @@ def booyer_moore(text, pattern):
                 j -= 1
         else:
             location = last[ord(text[i])]
-            print(i, j, location)
+            #print(i, j, location)
             i = i+m-min(j, 1+location)
             j = m-1
     return -1
@@ -70,20 +82,6 @@ def buildLast(pattern):
     for i in range(len(pattern)):
         last[ord(pattern[i])] = i
     return last
-
-
-def balesanBot(s):
-    if(re.search("masuk", s) != None):
-        return "Jadwal telah dimasukkan"
-    else:
-        temp = inputTask(s)
-        if(temp != ""):
-            return temp
-        elif(checkTask(s) != ""):
-            return checkTask(s)
-        else:
-            return "perintah tidak dikenali"
-        
 
 
 def inputTask(s):
@@ -122,7 +120,7 @@ def checkTask(s):
         return ""
     for i in arrayTugas:
         perkata = i.split(" - ")
-        print(perkata)
+        #print(perkata)
         if(deadline != -1 and tugas+pr+makalah != -3 and perkata[1] == matkul):
             if(tugas != -1 and perkata[2][0:2].lower() == "tu"):
                 return perkata[0]
@@ -157,23 +155,29 @@ def convertTanggal(s):
     else:
         return komponen[0] +"/" + "{0:02d}".format(bulan.index(komponen[1].upper()) + 1) + "/" + komponen[2][-2::] 
 
-
 def cekFromArray(s, array):
     for item in array:
         indeks = booyer_moore(s, item)
-        print(indeks)
+        #print(indeks)
         if(indeks != -1):
             return s[indeks:indeks+len(item)]
     return "ga ketemu"
 
-def createTask(tanggal,matakuliah,jenis,topik,arrayTugas):
-    tempTask = [tanggal,matakuliah,jenis,topik]
-    arrayTugas.append(tempTask)
-
-
+def printAllTask(arrayTugas):
+    temp = "List tugas yang tersimpan :\n "
+    count = 1
+    for item in arrayTugas:
+        temp += str(count) + ". "+item + "\n"
+        count+= 1
+    print("ini temporary print")
+    print(temp)
+    return temp
+    
+# def checkInputPrintTask(s):
 
 @app.route('/')
 def home():
+    chat.clear()
     return render_template('index.html', chat = chat)
 
 @app.route('/',methods=['POST'])
